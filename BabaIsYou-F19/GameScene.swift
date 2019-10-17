49ea99df3a8b5d51acbac9fa7ee294ec5e85d468
AJAY-MAC <letv4545@gmail.com>
2019-10-17 12:21:44 -0400

    Move Baba in for directions

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -11,8 +11,13 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
+    var baba:SKSpriteNode!
+    let player_speed:CGFloat = 20
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        
+        self.baba = self.childNode(withName: "baba") as! SKSpriteNode
+        
     }
    
     func didBegin(_ contact: SKPhysicsContact) {
@@ -24,6 +29,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let mouseTouch = touches.first
+        if (mouseTouch == nil) {
+            return
+        }
+        let location = mouseTouch!.location(in: self)
+        
+        // ----------------------------------------------
+        let nodeTouched = atPoint(location).name
+        
+        if (nodeTouched == "up") {
+            // move up
+            self.baba.position.y = self.baba.position.y + player_speed
+        }
+        else if (nodeTouched == "down") {
+            // move down
+            self.baba.position.y = self.baba.position.y - player_speed
+        }
+        else if (nodeTouched == "left") {
+            // move left
+            self.baba.position.x = self.baba.position.x - player_speed
+        }
+        else if (nodeTouched == "right") {
+            // move right
+            self.baba.position.x = self.baba.position.x + player_speed
+        }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
